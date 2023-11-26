81a42868a8e3e29eeb7400f5c4074cfea6818f58
Borislav Ivanov <slambeca@abv.bg>
2023-11-26 18:32:00 +0200

    Add files via upload

---
+++ 8.2. Advanced Queries in Django - Exercise/main_app/managers.py
@@ -0,0 +1,41 @@
+from decimal import Decimal
+from django.db import models
+from django.db.models import QuerySet, Count, Max, Min, Avg
+
+
+class RealEstateListingManager(models.Manager):
+    def by_property_type(self, property_type: str) -> QuerySet:
+        return self.filter(property_type=property_type)
+
+    def in_price_range(self, min_price: Decimal, max_price: Decimal) -> QuerySet:
+        return self.filter(price__gte=min_price, price__lte=max_price)
+        # return self.filter(price__range=(min_price, max_price))
+
+    def with_bedrooms(self, bedrooms_count: int) -> QuerySet:
+        return self.filter(bedrooms=bedrooms_count)
+
+    def popular_locations(self) -> QuerySet:
+        return self.values("location").annotate(
+            location_count=Count("location")
+        ).order_by("-location_count", "id")[:2]
+
+
+class VideoGameManager(models.Manager):
+    def games_by_genre(self, genre: str) -> QuerySet:
+        return self.filter(genre=genre)
+
+    def recently_released_games(self, year: int) -> QuerySet:
+        return self.filter(release_year__gte=year)
+
+    def highest_rated_game(self) -> QuerySet:
+        # return self.objects.all().order_by("-rating").first() but may not work
+        return self.annotate(max_rating=Max("rating")).order_by("-max_rating").first()
+
+    def lowest_rated_game(self) -> QuerySet:
+        # self.aggregate(min_rating=Min("rating"))["min_rating"]
+        # self.filter(rating=min_rating)
+        return self.annotate(lowest_rating=Min("rating")).order_by("lowest_rating").first()
+
+    def average_rating(self) -> str:
+        result = self.aggregate(average_rating=Avg("rating"))["average_rating"]
+        return f"{result:.1f}"
