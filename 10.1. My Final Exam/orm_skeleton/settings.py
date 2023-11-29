520cf9b9cfd7c245b81019ef587a9e47895e473f
Borislav Ivanov <slambeca@abv.bg>
2023-11-29 20:31:36 +0200

    Update settings.py

--- 10.1. My Final Exam/orm_skeleton/settings.py
+++ 10.1. My Final Exam/orm_skeleton/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'my_final_exam',
         'USER': 'postgres',
-        'PASSWORD': 'Fordmondeo7088',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
